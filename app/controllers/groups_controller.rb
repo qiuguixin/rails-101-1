945e0da3fb73e26dee30c9070a78f1e02c7b0f4c
qiuguixin <qguixin@qq.com>
2017-04-08 18:16:40 +0800

    fix bug "ActiveRecord::RecordNotFound in GroupsController#index"

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-  before_action :find_group_and_check_permission, [:edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -42,8 +42,10 @@ class GroupsController < ApplicationController
   end
 
   private
+
   def find_group_and_check_permission
     @group = Group.find(params[:id])
+
     if current_user != @group.user
       redirect_to root_path, alert: "You have no permission."
     end
